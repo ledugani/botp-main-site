b9763a7e6fd9301c7ded65fb1e3a31b74327b85c
Tom Dugan <thomas@aadwebb.com>
2020-09-20 13:39:23 -0500

    baseline project

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './impossible-cube.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <p className="spirit">BOTP</p>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className="body">
+          <em>will</em> be arriving soon...
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://instagram.com/anatomicalgift"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          data consumption
         </a>
       </header>
     </div>
